366ce7f6afd33e80f8be5f2a40138e31f4e38d92
Hamsa B C <hammugowda148@gmail.com>
2026-01-01 10:01:06 +0530

    fix vite build and deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Hamsa-B-C/',   // 👈 THIS IS THE FIX (repo name)
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
